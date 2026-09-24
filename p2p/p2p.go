9ed084b8dfceeae53bce684a6eba97417540af00
yecharlot <152829220+yecharlot@users.noreply.github.com>
2026-09-24 15:44:20 -0400

    Fijar DHT como servidor para red AlsetOS

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -100,7 +100,7 @@ func NuevoConEstado(identidadNodo *identidad.Identidad, escuchar, rutaOrganismos
 
 func iniciarDHT(ctx context.Context, nodo *Nodo) error {
 	if nodo.DHT != nil { return nil }
-	dht, err := kaddht.New(nodo.Host, kaddht.Datastore(nodo.datastore))
+	dht, err := kaddht.New(nodo.Host, kaddht.Datastore(nodo.datastore), kaddht.Mode(kaddht.ModeServer))
 	if err != nil { return fmt.Errorf("crear DHT: %w", err) }
 	nodo.DHT = dht
 	return nil
