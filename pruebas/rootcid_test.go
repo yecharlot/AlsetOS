97f5f7c845b8c53627f3a0e90dc08b247eca7fae
yecharlot <152829220+yecharlot@users.noreply.github.com>
2026-09-24 15:27:48 -0400

    pruebas: RootCID por contenido

--- pruebas/rootcid_test.go
+++ pruebas/rootcid_test.go
@@ -1,15 +1,28 @@
 package pruebas
 
 import (
-    "testing"
+	"testing"
 
-    "github.com/yecharlot/AlsetOS/rootcid"
+	"github.com/yecharlot/AlsetOS/rootcid"
 )
 
 func TestRootCIDDeterminista(t *testing.T) {
-    primero := rootcid.Crear("organismo-prueba")
-    segundo := rootcid.Crear("organismo-prueba")
-    if primero != segundo {
-        t.Fatalf("RootCID no determinista: %s != %s", primero, segundo)
-    }
+	primero := rootcid.Crear("organismo-prueba")
+	segundo := rootcid.Crear("organismo-prueba")
+	if primero != segundo {
+		t.Fatalf("RootCID no determinista: %s != %s", primero, segundo)
+	}
+}
+
+func TestRootCIDDependeDelContenido(t *testing.T) {
+	primero := rootcid.CrearContenido([]byte("organismo-a"))
+	segundo := rootcid.CrearContenido([]byte("organismo-a"))
+	tercero := rootcid.CrearContenido([]byte("organismo-b"))
+
+	if primero != segundo {
+		t.Fatalf("RootCID por contenido no determinista")
+	}
+	if primero == tercero {
+		t.Fatalf("contenidos distintos produjeron el mismo RootCID")
+	}
 }
