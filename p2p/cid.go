467e169291145bbbc95f35c0fc401fe797b8d0b6
yecharlot <152829220+yecharlot@users.noreply.github.com>
2026-09-24 15:44:44 -0400

    Mapear RootCID directamente a un CID de contenido

--- p2p/cid.go
+++ p2p/cid.go
@@ -1,19 +1,29 @@
 package p2p
 
 import (
+	"encoding/hex"
 	"fmt"
+	"strings"
 
 	"github.com/ipfs/go-cid"
 	multihash "github.com/multiformats/go-multihash"
 )
 
 func CIDRoot(rootCID string) (cid.Cid, error) {
-	if rootCID == "" {
-		return cid.Cid{}, fmt.Errorf("RootCID vacío")
+	rootCID = strings.TrimSpace(rootCID)
+	const prefijo = "rootcid:"
+	if !strings.HasPrefix(rootCID, prefijo) {
+		return cid.Cid{}, fmt.Errorf("RootCID inválido: %q", rootCID)
 	}
-	digest, err := multihash.Sum([]byte(rootCID), multihash.SHA2_256, -1)
+
+	digest, err := hex.DecodeString(strings.TrimPrefix(rootCID, prefijo))
+	if err != nil || len(digest) != 32 {
+		return cid.Cid{}, fmt.Errorf("digest RootCID inválido")
+	}
+
+	contenido, err := multihash.Encode(digest, multihash.SHA2_256)
 	if err != nil {
 		return cid.Cid{}, fmt.Errorf("crear multihash RootCID: %w", err)
 	}
-	return cid.NewCidV1(cid.Raw, digest), nil
+	return cid.NewCidV1(cid.Raw, contenido), nil
 }
